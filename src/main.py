7e7147f12ab97b70674ebf1fed53b362a0aba221
Yvan Varela <varelaivan30@gmail.com>
2022-08-20 08:04:58 -0400

    Add model and schema

--- src/main.py
+++ src/main.py
@@ -1,11 +1,37 @@
+# Pydantic
+from pydantic import BaseModel
+
+# FastApi
 from fastapi import FastAPI
+from fastapi import Body
 
 # Instancia de la clase
 app = FastAPI()
 
 
+# Models
+
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: str | None = None  # Optional
+    is_married: bool | None = None  # Optional
+
+
 # Path Operator Decoration
+
+
 @app.get('/')
 def home() -> dict:
     # return JSON
     return {'hello': 'word'}, 200
+
+
+# Request and Response Body
+
+
+@app.post('/person/')
+def add_person(person: Person = Body(...)):
+    return person
