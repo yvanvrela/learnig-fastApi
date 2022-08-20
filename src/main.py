3cf93d68632b6c9e777b494d856b9fb971fdfa39
Yvan Varela <varelaivan30@gmail.com>
2022-08-20 07:23:52 -0400

    Delete comments

--- src/main.py
+++ src/main.py
@@ -9,14 +9,3 @@ app = FastAPI()
 def home() -> dict:
     # return JSON
     return {'hello': 'word'}, 200
-
-
-"""
-    # Path Operations
-    Un path va despues del dominio -> mi-dominio.com/PATH/
-    Path -> Routes -> Endpoints     mi-dominio.com/API/
-
-    # Operations
-    Devuelve un header adicional llamado allow que contiene los metodos 
-    http que se utiliza en los endpoints.
-"""
