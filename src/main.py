a46afdcdd46b01bb6a189dabc564500a7e339a06
yvanvrela <varelaivan30@gmail.com>
2022-08-29 10:16:40 -0400

    httpexception

--- src/main.py
+++ src/main.py
@@ -10,13 +10,10 @@ from pydantic import EmailStr
 # FastApi
 # Sirve para definir el tipo de dato
 from fastapi import FastAPI
-from fastapi import Body
-from fastapi import Query
-from fastapi import Path
-from fastapi import Form
 from fastapi import status
-from fastapi import Header, Cookie
-from fastapi import UploadFile, File
+from fastapi import HTTPException
+from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
+
 
 # Instancia de la clase
 app = FastAPI()
@@ -172,10 +169,10 @@ async def show_person(
 
 # Validations: Query Parameters
 
-@app.get(
-    path='/person/detail/{person_id}',
-    status_code=status.HTTP_200_OK,
-)
+persons = [1, 2, 3, 4, 5]
+
+
+@app.get(path='/person/detail/{person_id}', status_code=status.HTTP_200_OK)
 async def show_person(
     person_id: int = Path(
         ...,
@@ -185,6 +182,11 @@ async def show_person(
         example=20
     )
 ):
+    if person_id not in persons:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="This person doesn't exist!"
+        )
     return {person_id: 'It exists!'}
 
 
